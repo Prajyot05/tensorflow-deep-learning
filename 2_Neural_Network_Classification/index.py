151e3f3a9eb6cf30a7965b7477bdb66a048b4373
Prajyot Tayde <pmtbmt@gmail.com>
2025-12-15 18:58:23 +0530

    restructure repo

---
+++ 2_Neural_Network_Classification/index.py
@@ -0,0 +1,9 @@
+'''
+What are classification problems?
+Teaching a model to sort items into distinct groups
+
+Type of classification problems:
+1. Binary classification (one thing or another)
+2. Multi-class classification (more than one thing or another)
+3. Multi-label classification (multiple label options per sample)
+'''
