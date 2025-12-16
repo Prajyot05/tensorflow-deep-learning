2022c82777cb4ad8b570179a7abb6d3148a51672
Prajyot Tayde <pmtbmt@gmail.com>
2025-12-16 16:12:27 +0530

    creating a basic classification model

--- 2_Neural_Network_Classification/index.py
+++ 2_Neural_Network_Classification/index.py
@@ -7,3 +7,35 @@ Type of classification problems:
 2. Multi-class classification (more than one thing or another)
 3. Multi-label classification (multiple label options per sample)
 '''
+
+from sklearn.datasets import make_circles
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# Make 1000 samples
+n_samples = 1000
+
+# Create circles
+X, y = make_circles(n_samples, noise=0.03, random_state=42)
+
+# Visualize the circles
+circles = pd.DataFrame({"X0": X[:, 0], "X1": X[:, 1], "label": y})
+plt.scatter(X[:, 0], X[:, 1], c = y, cmap = plt.cm.RdYlBu)
+
+tf.random.set_seed(42)
+
+# Creating the model
+model_1 = tf.keras.Sequential([
+    tf.keras.layers.Dense(1),
+    tf.keras.layers.Dense(1)
+])
+
+model_1.compile(loss = tf.keras.losses.BinaryCrossentropy(),
+                optimizer = tf.keras.optimizers.SGD(),
+                metrics=["accuracy"])
+
+model_1.fit(X, y, epochs=100, verbose=0)
+
+# Evaluating the model
+model_1.evaluate(X, y) # Terrible accuracy at this point
