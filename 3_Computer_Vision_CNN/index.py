8139acc7e8d6fb2a9b8cfb16cdd6f8387df1c3ba
Prajyot Tayde <pmtbmt@gmail.com>
2026-01-10 22:44:36 +0530

    visualizing the data

--- 3_Computer_Vision_CNN/index.py
+++ 3_Computer_Vision_CNN/index.py
@@ -39,3 +39,42 @@ import os
 # Walk through the pizza_steak directory and list number of files
 for dirpath, dirnames, filenames in os.walk("pizza_steak"):
   print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+
+# Another way to find out how many images are in a file
+num_steak_images_train = len(os.listdir("pizza_steak/train/steak"))
+print(num_steak_images_train)
+
+# To visualize images first let's get the class names programmatically
+import pathlib
+import numpy as np
+data_dir = pathlib.Path("pizza_steak/train/")
+class_names = np.array(sorted([item.name for item in data_dir.glob('*')])) # Created a list of class names from the sub directories
+print(class_names)
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import random
+
+def view_random_image(target_dir, target_class):
+  # Setup target directory (we'll view images from here)
+  target_folder = target_dir+target_class
+
+  # Get a random image path
+  random_image = random.sample(os.listdir(target_folder), 1)
+
+  # Read in the image and plot it
+  img = mpimg.imread(target_folder + "/" + random_image[0])
+  plt.imshow(img)
+  plt.title(target_class)
+  plt.axis("off")
+
+  print("Image Shape: ", img.shape)
+  return img
+
+img = view_random_image(target_dir="pizza_steak/train/", target_class="steak")
+
+# The img returned is actuall an array (that can easily be converted into a tensor)
+print(img)
+
+# View the image shape
+print(img.shape) # Returns the width, height and color channels
