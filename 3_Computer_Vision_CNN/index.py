71b12a3bc83940fcb6a03a341a826c372712c52c
Prajyot Tayde <pmtbmt@gmail.com>
2026-02-16 22:08:38 +0530

    visualizing the model predictions

--- 3_Computer_Vision_CNN/index.py
+++ 3_Computer_Vision_CNN/index.py
@@ -613,5 +613,34 @@ Our model was trained in batches, so the batch size becomes the first dimension.
 So in reality, our model was trained on data in the shape of (batch_size, 224, 224, 3).
 We can fix this by adding an extra to our custom image tensor using tf.expand_dims.
 '''
-model_7.predict(tf.expand_dims(steak, axis=0))
+pred = model_7.predict(tf.expand_dims(steak, axis=0))
 # This gives us the prediction probability (how likely the image is to belong to one class or the other)
+
+# Now let's try to visualize the image as well as the model's prediction
+print(class_names)
+
+# We can index the predicted class by rounding the prediction probability
+pred_class = class_names[int(tf.round(pred)[0][0])]
+print(pred_class)
+
+def pred_and_plot(model, filename, class_names):
+  '''
+  Imports an image located at filename, makes a prediction on it with
+  a trained model and plots the image with the predicted class as the title.
+  '''
+  # Import the target image and preprocess it
+  img = load_and_prep_image(filename)
+
+  # Make a prediction
+  pred = model.predict(tf.expand_dims(img, axis=0))
+
+  # Get the predicted class
+  pred_class = class_names[int(tf.round(pred)[0][0])]
+
+  # Plot the image and predicted class
+  plt.imshow(img)
+  plt.title(f"Prediction: {pred_class}")
+  plt.axis(False)
+
+# Testing our function
+pred_and_plot(model_7, "03-steak.jpeg", class_names)
