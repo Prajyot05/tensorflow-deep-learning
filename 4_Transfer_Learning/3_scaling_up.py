9bddfe546eff104caed83aaf1e1fe1703c828a0a
Prajyot Tayde <pmtbmt@gmail.com>
2026-04-03 16:47:27 +0530

    intro to scaling up and setting up notebook

---
+++ 4_Transfer_Learning/3_scaling_up.py
@@ -0,0 +1,9 @@
+'''
+Now we're going to scale up from using 10 classes of the Food101 data to using all of the classes in the Food101 dataset.
+'''
+
+# Check if we are using a GPU
+# !nvidia-smi
+
+# Get helper functions file
+# !wget https://raw.githubusercontent.com/mrdbourke/tensorflow-deep-learning/main/extras/helper_functions.py
