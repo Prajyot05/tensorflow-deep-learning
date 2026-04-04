4bb6d4ec520c4f5ddd5c2e46f09a8ba809e35a8e
Prajyot Tayde <pmtbmt@gmail.com>
2026-04-04 22:47:10 +0530

    get common helper functions and test data

--- 4_Transfer_Learning/3_scaling_up.py
+++ 4_Transfer_Learning/3_scaling_up.py
@@ -7,3 +7,29 @@ Now we're going to scale up from using 10 classes of the Food101 data to using a
 
 # Get helper functions file
 # !wget https://raw.githubusercontent.com/mrdbourke/tensorflow-deep-learning/main/extras/helper_functions.py
+
+# Import all commonly used helper functions
+from helper_functions import create_tensorboard_callback, plot_loss_curves, unzip_data, compare_historys, walk_through_dir
+
+# Download data
+# !wget https://storage.googleapis.com/ztm_tf_course/food_vision/101_food_classes_10_percent.zip 
+
+unzip_data("101_food_classes_10_percent.zip")
+
+train_dir = "101_food_classes_10_percent/train/"
+test_dir = "101_food_classes_10_percent/test/"
+
+# How many images/classes are there?
+walk_through_dir("101_food_classes_10_percent")
+
+# Setup data inputs
+import tensorflow as tf
+IMG_SIZE = (224, 224)
+train_data_all_10_percent = tf.keras.preprocessing.image_dataset_from_directory(train_dir,
+                                                                                label_mode="categorical",
+                                                                                image_size=IMG_SIZE)
+                                                                                
+test_data = tf.keras.preprocessing.image_dataset_from_directory(test_dir,
+                                                                label_mode="categorical",
+                                                                image_size=IMG_SIZE,
+                                                                shuffle=False) # we're not shuffling because we want to use the order later
